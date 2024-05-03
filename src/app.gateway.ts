04484dd11ec6d8afb59d3d12cfb3c48c112b3e7d
jonabergamo <jonathanbergamo16@gmail.com>
2024-05-03 11:41:50 -0300

    feat: add socket dinamic topic

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,24 +1,38 @@
-import {
-  WebSocketGateway,
-  WebSocketServer,
-  OnGatewayConnection,
-  OnGatewayDisconnect,
-} from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { WebSocketGateway, WebSocketServer, SubscribeMessage, OnGatewayConnection, OnGatewayDisconnect, ConnectedSocket, MessageBody } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway({ cors: true })
+@WebSocketGateway({
+  cors: {
+    origin: "*", // Ajuste isso conforme sua política de CORS
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+})
 export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
 
-  handleConnection(client: any) {
+  handleConnection(client: Socket) {
     console.log(`Client connected: ${client.id}`);
   }
 
-  handleDisconnect(client: any) {
+  handleDisconnect(client: Socket) {
     console.log(`Client disconnected: ${client.id}`);
   }
 
-  broadcastMessage(topic: string, message: string) {
-    this.server.emit(topic, message);
+  @SubscribeMessage('subscribe')
+  handleSubscribe(@ConnectedSocket() client: Socket, @MessageBody() data: {topic:string}): void {
+      console.log("Received data:", data); // Confirma que os dados estão corretos
+      console.log(`Subscribing client ${client.id} to topic ${data.topic}`);
+      client.join('device/teste');
   }
+
+  @SubscribeMessage('unsubscribe')
+  handleUnsubscribe(@ConnectedSocket() client: Socket, @MessageBody() data: { topic: string }) {
+    console.log(`Unsubscribing ${client.id} from topic ${data.topic}`);
+    client.leave(data.topic);
+  }
+
+  broadcastMessage(topic: string, message: string) {
+    this.server.to(topic).emit(topic, message);
+}
 }
